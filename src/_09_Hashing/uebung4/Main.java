f8e8d5775ea1ec65a257374fbabac74b094ef2cb
MyUsernameDDM <stmaydam@bx.fallmerayer.it>
2023-03-17 10:50:17 +0100

    HashFUnktion

--- src/_09_Hashing/uebung4/Main.java
+++ src/_09_Hashing/uebung4/Main.java
@@ -6,6 +6,21 @@ import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Main {
+
+    static private int hash(Student student, int m){
+        String firstName = student.firstName;
+        String lastName = student.lastName;
+
+        int sum = 0;
+        for (int i = 0; i < firstName.length(); i++) {
+            sum += firstName.charAt(i) % 30;
+        }
+        for (int i = 0; i < firstName.length(); i++) {
+            sum += lastName.charAt(i) % 30;
+        }
+
+        return sum % m;
+    }
     public static void main(String[] args) {
         LinkedList<Integer>  l = new LinkedList<>();
         l.push(3);
@@ -15,5 +30,6 @@ public class Main {
         LinkedList<Student>[] hashMap = new LinkedList[28];
         Scanner s = new Scanner(System.in);
         System.out.println(s.nextLine());
+
     }
 }
