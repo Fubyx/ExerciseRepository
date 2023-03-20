d023ce77154a2008aff2cf5663da6a121677e343
Fubyx <fabireif11@gmail.com>
2023-03-20 15:23:37 +0100

    mains

---
+++ src/_10_Collections/uebung1/Main.java
@@ -0,0 +1,4 @@
+package _10_Collections.uebung1;
+
+public class Main {
+}
---
+++ src/_10_Collections/uebung2/Main.java
@@ -0,0 +1,7 @@
+package _10_Collections.uebung2;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
