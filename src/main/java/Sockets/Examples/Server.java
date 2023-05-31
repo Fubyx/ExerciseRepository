ef35742943a3f98056e16aeaadcd8dec4e8f6b84
Fubyx <fabireif11@gmail.com>
2023-05-31 14:15:06 +0200

    added server to socket

---
+++ src/main/java/Sockets/Examples/Server.java
@@ -0,0 +1,23 @@
+package Sockets.Examples;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(50000);
+        Socket client = serverSocket.accept();
+
+        while (true) {
+            Scanner in = new Scanner(client.getInputStream());
+            String text = in.nextLine();
+            System.out.println(text);
+            PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+            out.println("twfdtwfdujwfdzwd" + text);
+        }
+    }
+}
+
