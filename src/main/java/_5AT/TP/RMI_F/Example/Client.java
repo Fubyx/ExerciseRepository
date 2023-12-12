1acfb355fa8b2d4e7b308242b9de60813082f769
Fubyx <fabireif11@gmail.com>
2023-12-12 19:27:50 +0100

    Working policy file

--- src/main/java/_5AT/TP/RMI_F/Example/Client.java
+++ src/main/java/_5AT/TP/RMI_F/Example/Client.java
@@ -6,7 +6,7 @@ import java.rmi.registry.Registry;
 public class Client {
     public static void main(String[] args) {
         try {
-            Registry registry = LocateRegistry.getRegistry("127.0.0.1", 50535);
+            Registry registry = LocateRegistry.getRegistry("127.0.0.1", 1099);
             Text t_stub = (Text) registry.lookup("TextMessage");
             String response = t_stub.getText();
             System.out.println("Antwort vom Server: " + response);
